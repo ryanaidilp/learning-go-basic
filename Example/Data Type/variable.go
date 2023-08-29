7aa39d43e2396c83fe1584a5736d2cb2c2953a8c
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-08-29 12:13:36 +0800

    chore(notes): add notes and example for variable

---
+++ Example/Data Type/variable.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var isTrue bool
+
+	isTrue = true
+
+	fmt.Println(isTrue)
+
+	var (
+		name = "My Name"
+		age  = 24
+	)
+
+	fmt.Println(name)
+	fmt.Println(age)
+
+}
